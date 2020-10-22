cd0a0fd4da57e0fc8952ef05c8e09eb9d2ab363a
shikhar0507 <kap.shikhar@gmail.com>
2020-10-22 12:44:04 +0530

    users folder init

--- public/admin/users/add.js
+++ public/admin/users/add.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('manage-form');
 const submitBtn = form.querySelector('.form-actionable .mdc-fab--action[type="submit"]')
-const init = (office,officeId) => {
+
+const init = (office, officeId) => {
     
 }
---
+++ public/admin/users/update.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('manage-form');
+const submitBtn = form.querySelector('.form-actionable .mdc-fab--action[type="submit"]')
+
+const init = (office, officeId) => {
+    
+}
